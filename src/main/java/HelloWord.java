1e84c9e31d0441dc94d4ec6afc0b0970dd421f1b
root <529421683@qq.com>
2018-10-19 18:51:33 +0800

    这是dev第一次保存

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -3,7 +3,7 @@
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("这是第一次保存");
-        System.out.println("dev第一次保存");
+        System.out.println("这是第一次fdg 保存");
+        System.out.println("master修改");
     }
 }
