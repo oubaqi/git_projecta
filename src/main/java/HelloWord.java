961f714b69fd1616c7788db6e81569f84d1e69ee
root <529421683@qq.com>
2018-10-21 09:47:42 +0800

    这是一个周日

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -5,5 +5,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("这是dev修改");
         System.out.println("这是一个dev");
+        System.out.println("今天周日，心情好");
     }
 }
